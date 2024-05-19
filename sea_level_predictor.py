ecdfe14458c077942c2c774ee1703729fb89286d
vijaybhaskar-123 <vijaybhaskar199912@gmail.com>
2024-05-19 10:41:06 +0000

    first commit

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -12,7 +12,7 @@ def draw_plot():
 
     # Create first line of best fit
     firstline_model=linregress(df['Year'],df['CSIRO Adjusted Sea Level'])
-    x_pred=pd.Series([i for i in range(1880,2051)])
+    x_pred=pd.Series(range(1880,2051))
 
     first_line=firstline_model.slope*x_pred+firstline_model.intercept
     plt.plot(x_pred, first_line)
@@ -20,8 +20,8 @@ def draw_plot():
     # Create second line of best fit
     datafrom2000year=df[df['Year']>=2000]
     second_line_model=linregress(datafrom2000year['Year'],datafrom2000year['CSIRO Adjusted Sea Level'])
-    Second_line=second_line_model.slope*pd.Series([i for i in range(2000,2051)])+second_line_model.intercept
-    plt.plot(pd.Series([i for i in range(2000,2051)]), Second_line)
+    Second_line=second_line_model.slope*pd.Series(range(2000, 2051))+second_line_model.intercept
+    plt.plot(pd.Series(range(2000, 2051)), Second_line)
 
     # Add labels and title
 
